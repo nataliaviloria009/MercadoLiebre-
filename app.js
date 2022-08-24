90ecb05eadaa6c3994f3d59ceb189103e8b26520
nataliaviloria009 <108700201+nataliaviloria009@users.noreply.github.com>
2022-08-24 22:19:36 +0200

    puerto 3000 !

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
     console.log('Server is running on port 3000');
 })
 
